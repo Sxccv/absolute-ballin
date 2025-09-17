f27d03f1e58846cb9b280f6c66062cef99d6afd5
Sxccv <aosfnmonaos@gmail.com>
2025-09-17 11:11:37 +0700

    THE ACTUAL SELESAI TUGAS 3

--- absolute_ballin/settings.py
+++ absolute_ballin/settings.py
@@ -34,7 +34,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "randuichi-touya-footballnews.pbp.cs.ui.ac.id","randuichi-touya-absoluteballin.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://randuichi-touya-footballnews.pbp.cs.ui.ac.id/"
+    "https://randuichi-touya-absoluteballin.pbp.cs.ui.ac.id/"
 ]
 
 # Application definition
